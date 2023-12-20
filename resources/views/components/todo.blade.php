fd31bbaba557fc95f79679f0988fbb3bbb7067f9
LucasJR13 <lucjuariq@alu.edu.gva.es>
2023-12-20 20:31:08 +0100

    traducir botones al ingles

--- resources/views/components/todo.blade.php
+++ resources/views/components/todo.blade.php
@@ -7,13 +7,13 @@
             <form action="{{ route('todo.destroy', $todo->id) }}" method="POST" class="flex ml-2">
                 @csrf
                 @method('DELETE')
-                <x-button color="red" small>borrar</x-button>
+                <x-button color="red" small>delete</x-button>
             </form>
 
             <form action="{{ route('todo.change-is-done', $todo->id) }}" method="POST" class="flex ml-2">
                 @csrf
                 <x-button :color="$todo->is_done ? 'gray' : 'blue'" small>
-                    {{ $todo->is_done ? 'Marcar como no hecho' : 'Marcar como hecho' }}
+                    {{ $todo->is_done ? 'Mark as undone' : 'Mark as done' }}
                 </x-button>
             </form>
         </div>
