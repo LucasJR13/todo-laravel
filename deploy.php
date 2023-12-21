21412587477df46b5df017f242375e60d110d423
Andreu Puchades <andpucpas@alu.edu.gva.es>
2023-12-21 14:31:21 +0100

    Andreu commit

--- deploy.php
+++ deploy.php
@@ -22,8 +22,7 @@ add('writable_dirs', ['storage', 'boostrap/cache']);
 
 // Hosts
 
-host('172.16.221.184')
-    ->user('prod-ud4-deployer')
+host('172.16.221.184') ->user('prod-ud4-deployer')
     ->identityFile('~/.ssh/id_rsa')
     ->set('deploy_path', '/var/www/prod-ud4-a4/html');
 
